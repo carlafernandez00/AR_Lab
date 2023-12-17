279585fcf2f916f7ef73d5f84150270266ad6d66
CrowdMedia <crowdmedia@CrowdMedias-MacBook-Pro.local>
2023-12-17 23:52:13 +0100

    object tracking and manipulation

---
+++ Library/Bee/artifacts/iOS/il2cppOutput/cpp/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+IL2CPP_EXTERN_C_CONST int32_t* g_FieldOffsetTable[];
+IL2CPP_EXTERN_C_CONST Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	8910,
+	g_Il2CppGenericTypes,
+	5468,
+	g_Il2CppGenericInstTable,
+	68070,
+	g_Il2CppGenericMethodFunctions,
+	25370,
+	g_Il2CppTypeTable,
+	81124,
+	g_Il2CppMethodSpecTable,
+	7490,
+	g_FieldOffsetTable,
+	7490,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
